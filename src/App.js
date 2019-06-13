c09e349ca52ca417672612377fa0ffc40e844d12
Squaaw <damien.jisseau@edu.itescia.fr>
2019-06-13 16:21:41 +0200

    FIX LOGO LA PUTAIN DE TA DARONNE

--- src/App.js
+++ src/App.js
@@ -2,28 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import List from "./components/list/list";
 // import gameDetails from "./components/games-details/games-details";
-import { getGamesNameList, getGamesCoverList } from "./services/gameListClient";
+//import { getGamesNameList, getGamesCoverList } from "./services/gameListClient";
 
 export default class App extends Component {
-  async componentDidMount() {
-    const gamesNameList = await getGamesNameList("games");
-    const gamesCoverList = await getGamesCoverList("covers");
-    console.log("App.render ===>", gamesNameList);
-    this.setState({
-      gamesNameList,
-      gamesCoverList
-    });
-  }
+  // async componentDidMount() {
+  //   const gamesNameList = await getGamesNameList("games");
+  //   const gamesCoverList = await getGamesCoverList("covers");
+  //   console.log("App.render ===>", gamesNameList);
+  //   this.setState({
+  //     gamesNameList,
+  //     gamesCoverList
+  //   });
+  // }
   render() {
     // const {gamesList} = this.state
     // console.log('App.render ===>',gamesList)
     return (
       <div className="App">
         <header className="App-header">
-          <List />
-          <gameDetails />
-          LA PICHE GAMES'
-          <List />
           <img
             id="logo"
             src="https://st3.depositphotos.com/9866364/19226/i/1600/depositphotos_192261768-stock-photo-laptop-gamepad-headphones-potato-chips.jpg"
