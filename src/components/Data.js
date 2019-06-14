bb4709e157d8ec966c6edeecda327666a308d946
Doryan Lièvre <doryan.lievre@edu.itescia.fr>
2019-06-14 15:26:32 +0200

    button show

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,17 +5,57 @@ export default class Data extends Component {
   static defaultProps = {
     datas: []
   }
+  state = {
+    show: false
+  }
+
+  toggleTitle() {
+    const {show} = this.state
+
+    this.setState({
+      show: !show
+    })
+  }
+
+  renderTime(datas) {
+    const {show} = this.state
+
+    if (!show) {
+      return
+    }
+    return (
+      <div>
+          IMAGE
+      </div>
+    )
+  }
+
+
+  renderList() {
+    const {datas} = this.props
+    return datas.map(data => (
+      <li key={data.id}>
+        <div>
+          <a href={data.url}>{data.name}</a>
+        </div>
+        {this.renderTime(datas)}
+      </li>
+   
+     ))
+  }
+
 
   render () {
-    const {datas} = this.props
-    return (
-      <ul>
-        {datas.map(data => (
-          <li key={data.id}>
-            <a href={data.url}>{data.name}</a>
-          </li>
-        ))}
-      </ul>
+    const {show} = this.state
+    return (  
+      <div>
+        <button onClick={this.toggleTitle.bind(this)}>
+          {show ? 'Hide' : 'Show'}
+        </button>
+          <ul>
+            {this.renderList()}
+          </ul>
+      </div>
     )
   }
 }
