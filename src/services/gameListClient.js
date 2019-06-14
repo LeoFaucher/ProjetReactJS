93a2b9996060a2ae3676cc3cf8fe9c754402d730
Unknown <leo.faucher@edu.itescia.fr>
2019-06-14 13:25:25 +0200

    fix fields details

--- src/services/gameListClient.js
+++ src/services/gameListClient.js
@@ -39,7 +39,7 @@ export async function getGamesDetailsList() {
             'Accept': 'application/json',
             'user-key': API_KEY
         },
-        data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,collection,cover,created_at,dlcs,expansions,external_games,first_release_date,follows,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; limit 50;"
+        data: "fields *; limit 50;"
     })
     return data
 }
